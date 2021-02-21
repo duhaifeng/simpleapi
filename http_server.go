9550c4f6134f29147d78744e39949aa7f3f76780
duhaifeng <duhaifeng@jd.com>
2021-02-21 12:49:46 +0800

    增加将POST请求的Body JSON数据注入Handler字段的能力

--- http_server.go
+++ http_server.go
@@ -1,6 +1,7 @@
 package simpleapi
 
 import (
+	"encoding/json"
 	"fmt"
 	log "github.com/duhaifeng/loglet"
 	"github.com/duhaifeng/simpleapi/db"
@@ -150,14 +151,17 @@ func (this *ApiServer) registerStructHandlerRoute() {
 			this.GetTokenFunnel().GetToken(r.URL.Path, ctx)
 			//每次请求需要生成一个新的Handler对象，避免上下文对象被多个请求共享
 			newStructHandlerVal := reflect.New(structHandlerType)
+			if r.Method == http.MethodPost && r.Header.Get("Content-Type") == "application/json" {
+				//将Body的JSON数据组装到Handler数据字段中
+				this.assembleRequestDataToHandler(newStructHandlerVal, reqWrapper)
+			}
 			//组装Handler内声明的所有Service Field
 			newStructHandlerVal = this.assembleServiceToHandler(newStructHandlerVal, ctx)
-			//TODO: 如果是POST请求，自动将BODY数据映射到对象中
 			newStructHandler := newStructHandlerVal.Interface().(IApiHandler)
 			newStructHandler.setContext(ctx)
 			newStructHandler.setReqAndResp(reqWrapper, respWrapper)
 			newStructHandler.Init()
-			headerInterceptor := this.assembleInterceptors(newStructHandler, ctx)
+			headerInterceptor := this.assembleInterceptors(newStructHandler, ctx, reqWrapper, respWrapper)
 			this.callStructHandler(headerInterceptor, ctx, reqWrapper, respWrapper)
 		}
 		logger.Debug("register api struct handler: %d <%s> %s %s", i, handlerDef.Method, handlerDef.Path, structHandlerType.String())
@@ -168,14 +172,55 @@ func (this *ApiServer) registerStructHandlerRoute() {
 /**
  * 基于HTTP请求构造请求上下文对象
  */
-func (this *ApiServer) constructContext(req *Request) *RequestContext {
+func (this *ApiServer) constructContext(r *Request) *RequestContext {
 	ctx := new(RequestContext)
 	ctx.Init()
-	ctx.SetUserToken(req.GetHeader(HTTP_HEADER_AUTH_TOKEN))
-	ctx.SetRequestId(req.GetHeader(HTTP_HEADER_REQ_IDENTIFIER))
-	ctx.SetClientFlag(req.GetHeader(HTTP_HEADER_CLIENT_FALG))
-	ctx.SetClientIp(req.GetOriReq().RemoteAddr)
+	ctx.SetUserToken(r.GetHeader(HTTP_HEADER_AUTH_TOKEN))
+	ctx.SetRequestId(r.GetHeader(HTTP_HEADER_REQ_IDENTIFIER))
+	ctx.SetClientFlag(r.GetHeader(HTTP_HEADER_CLIENT_FALG))
+	ctx.SetClientIp(r.GetOriReq().RemoteAddr)
 	return ctx
+}
+
+/**
+ * 将请求数据从HTTP Body封装到Handler的数据对象中
+ */
+func (this *ApiServer) assembleRequestDataToHandler(handlerVal reflect.Value, r *Request) reflect.Value {
+	body, err := r.GetBody()
+	if err != nil {
+		logger.Error("get body json data failed: %s", err.Error())
+		return handlerVal
+	}
+	handlerElem := handlerVal.Elem()
+	for i := 0; i < handlerElem.NumField(); i++ {
+		handlerFieldVal := handlerElem.Field(i)
+		if !handlerFieldVal.IsValid() || !handlerFieldVal.CanInterface() {
+			continue
+		}
+		handlerFieldType := handlerFieldVal.Type()
+		if handlerFieldType.Kind() != reflect.Ptr {
+			continue
+		}
+		//Go中父类也会被子类当成Field遍历出来，需要跳过对父类属性的重生成
+		if strings.Contains(handlerFieldType.String(), "BaseHandler") {
+			continue
+		}
+		//获取Handler中声明的公开变量类型，并通过反射的方式为其注入实例
+		fieldVal := reflect.New(handlerFieldType.Elem())
+		_, ok := fieldVal.Interface().(IApiService)
+		//跳过对Service字段的处理
+		if ok {
+			continue
+		}
+		filedObj := fieldVal.Interface()
+		err := json.Unmarshal(body, filedObj)
+		if err != nil {
+			logger.Error("can not inject body json data to filed: %s", err.Error())
+			continue
+		}
+		handlerFieldVal.Set(fieldVal)
+	}
+	return handlerVal
 }
 
 /**
@@ -239,7 +284,7 @@ func (this *ApiServer) assembleDbToService(serviceObj IApiService, serviceVal re
 			continue
 		}
 		//logger.Debug("%s assemble db operator <%p-%s> to service <%p-%s>'s field", ctx.GetRequestId(), &dbObj, serviceFieldType.Name(), &serviceObj, serviceElem.Type().Name())
-		dbObj.SetContext(ctx)
+		dbObj.setContext(ctx)
 		//为了确保一个Service下所有的DB操作属于一个事务，因此需要让DB对象反引Service对象，并从中获取DB连接
 		dbObj.SetService(&serviceObj)
 		dbObj.Init()
@@ -251,7 +296,7 @@ func (this *ApiServer) assembleDbToService(serviceObj IApiService, serviceVal re
 /**
  * 为一个Handler组装拦截器链
  */
-func (this *ApiServer) assembleInterceptors(handler IApiHandler, ctx *RequestContext) IApiHandler {
+func (this *ApiServer) assembleInterceptors(handler IApiHandler, ctx *RequestContext, r *Request, w *Response) IApiHandler {
 	var newInterceptors []IApiHandler
 	//为了防止拦截器对象复用造成数据安全问题，所以每次请求Handler对象的关联拦截器都重新生成一份拦截器实例
 	for _, interceptor := range this.interceptors {
@@ -259,6 +304,7 @@ func (this *ApiServer) assembleInterceptors(handler IApiHandler, ctx *RequestCon
 		newInterceptorVal := reflect.New(interceptorType.Elem())
 		newInterceptor := newInterceptorVal.Interface().(IApiHandler)
 		newInterceptor.setContext(ctx)
+		newInterceptor.setReqAndResp(r, w)
 		newInterceptor.Init()
 		newInterceptors = append(newInterceptors, newInterceptor)
 	}
